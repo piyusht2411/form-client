4108e775a32744d377b4703f5ab77c7253af49dc
Piyush Thakur <piyushthakur241199@gmail.com>
2024-01-27 10:34:02 +0530

    logout feature

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import { useAppSelector } from '../store/store'
-import { selectAuth } from '../store/reducer/authSlice'
+import { useAppDispatch, useAppSelector } from '../store/store'
+import { logout, selectAuth } from '../store/reducer/authSlice'
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
     const {name} = useAppSelector(selectAuth);
     const navigate = useNavigate();
+    const dispatch = useAppDispatch();
+    const handleLogout = ()=>{
+      dispatch(logout());
+      navigate('/');
+
+    }
   return (
    <>
    <h2>Welcome to dashboard</h2>
    <h4>Name:{name}</h4>
+   <button onClick={()=>handleLogout()}>logout</button>
 
    </>
   )
